41fe413172cfa7cba892aae764dc04daf24671b6
chuks <alexanderchuks310@gmail.com>
2023-02-25 11:14:26 +0100

    changed picstures names

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,5 +1,13 @@
 <template>
-    <b>hello</b>
+    <div class="container mx-auto m-6 flex  bg-slate-300 max-screen-md">
+        <img src="../assets/logoweb.png" />
+        <b class="m-4">Scms</b>
+        
+       
+        <ul class="flex">
+            <li></li>
+        </ul>
+    </div>
 </template>
 
 <script>
